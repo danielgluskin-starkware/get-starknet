ad064d0037a45840f598f3305ce8776496629f3b
Daniel Gluskin <110768512+danielgluskin-starkware@users.noreply.github.com>
2024-05-07 15:28:53 +0300

    feature 2 (#13)
    feat

--- packages/ui/src/modal/index.ts
+++ packages/ui/src/modal/index.ts
@@ -50,7 +50,7 @@ export default async function show({
       ...preAuthorizedWallets,
     ])
 
-    console.log("EEE")
+    console.log("EEE!!!")
 
     const modal = new Modal({
       target: document.body,
